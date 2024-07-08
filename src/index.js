784951213bb000955640adfa8044eea130730404
imtiaz0212 <imtiaz0212@gmail.com>
2024-07-08 11:34:16 +0600

    cloudinary and multer install and config

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import dotenv from "dotenv"
 import connectDB from "./db/config.js"
 import {app} from "./app.js"
 
+import bcrypt from "bcrypt"
+
 dotenv.config({
     path: "./env"
 });
 
 const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello World! Test')
+  })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
