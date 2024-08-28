62c34b4e5167c00b1b791acb1db6859296a6fb38
Imtiaz <imtiaz0212@gmail.com>
2024-08-28 15:06:52 +0600

    add proxy

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import path from "path"
 
 const app = express()
 
+//  Set favicon
 app.use(favicon(path.resolve('./public/favicon.ico')))
 
 // Set Access Origin
@@ -22,8 +23,8 @@ app.use(helmet())
 // Limit requests from same API
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    limit: 500,
-    message: 'Too many requests from this IP, please try again'
+    limit: 200,
+    message: 'Too many requests from this IP, please try after some time again.'
 });
 app.use(limiter);
 
@@ -42,9 +43,6 @@ import employeeRoute from "./routes/employeeRoute.js"
 import shiftRoute from "./routes/shiftRoute.js"
 import companyRoute from "./routes/companyRoute.js"
 import designationRoute from "./routes/designationRoute.js"
-
-
-
 
 
 
