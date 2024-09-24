518629e8ea594752f9f11ac3c607b590e056b264
Imtiaz <imtiaz0212@gmail.com>
2024-09-24 17:33:56 +0600

    Update middlewate

--- src/routes/projectRoute.js
+++ src/routes/projectRoute.js
@@ -14,19 +14,19 @@ const route = Router();
 
 route
     .route("/project")
-    .get(getAllData)
-    .post(upload.single("projectImage"), createData);
+    .get(authCheck, getAllData)
+    .post(authCheck, upload.single("projectImage"), createData);
 
-route.route("/project/ongoing").get(getAllData);
-route.route("/project/onhold").get(getAllData);
-route.route("/project/completed").get(getAllData);
-route.route("/project/canceled").get(getAllData);
-route.route("/project/count").get(getCountData);
+route.route("/project/ongoing").get(authCheck, getAllData);
+route.route("/project/onhold").get(authCheck, getAllData);
+route.route("/project/completed").get(authCheck, getAllData);
+route.route("/project/canceled").get(authCheck, getAllData);
+route.route("/project/count").get(authCheck, getCountData);
 
 route
     .route("/project/:id")
-    .get(getData)
-    .patch(upload.single("projectImage"), updateData)
-    .delete(deleteData);
+    .get(authCheck, getData)
+    .patch(authCheck, upload.single("projectImage"), updateData)
+    .delete(authCheck, deleteData);
 
 export default route;
