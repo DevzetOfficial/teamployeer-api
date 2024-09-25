d980d510571a8d778ede14c20b51d8216ac7103a
Imtiaz <imtiaz0212@gmail.com>
2024-09-25 11:13:57 +0600

    udpate project route

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -16,7 +16,7 @@ const authCheck = asyncHandler(async (req, _, next) => {
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         const user = await User.findById(decodedToken?._id).select(
-            "-password -refreshToken"
+            "-googleId -refreshToken"
         );
 
         if (!user) {
